072303f9aecdd7f7db6319f76f20bba9a40e0951
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-11-24 14:48:43 +0100

    feat: Add type-annotated to_str function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for float-to-string conversion"""
+
+
+def to_str(n: float) -> str:
+    """Convert a floating-point number to its string representation"""
+
+    # Use the str function to convert the float to string
+    return str(n)
