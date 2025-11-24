a9ea0f128b23f9c271ba317874594ab6d32c5384
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-11-24 14:29:12 +0100

    feat: Add type-annotated function for string concatenation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a function for string concatenation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Contatenate two strings and return the result"""
+
+    return str1 + str2
