8c6394d9ae189c8b803bdf531db02f232368d612
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-11-24 14:21:09 +0100

    feat: Add type-annotated function add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a function for adding two floats"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating-point numbers and return their sum"""
+
+    return a + b
