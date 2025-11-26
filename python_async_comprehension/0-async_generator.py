a225705f7d34789fdd04159ce8c7cbda6f373156
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-11-26 13:17:56 +0100

    feat: Add async_generator coroutine

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for ans asynchronous generator yielding random number"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Asynchronous generator that yields 10 random values between 0 & 10,
+    waiting 1 sec before yielding each value"""
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
+async def test_gen():
+    result = []
+    async for val in async_generator():
+        result.append(val)
+    print(result)
+
+asyncio.run(test_gen())
