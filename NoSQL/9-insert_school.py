955ee1246d6ccdee20e54afc0bb662698255864d
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-12-04 14:34:14 +0100

    feat: Add insert_school function to MongoDB

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,8 +2,8 @@
 """Module for insertinf a new dcument in a MongoDB collection"""
 
 
-def insert_scholl(mongo_collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """Insert a new document in collection & return its _id"""
 
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    doc_id = mongo_collection.insert_one(kwargs).inserted_id
+    return doc_id
