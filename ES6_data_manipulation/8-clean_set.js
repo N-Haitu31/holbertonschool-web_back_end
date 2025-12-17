1cfcd20f9a32cbe6ffd2211da97e9a2381db29ab
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-12-17 21:42:40 +0100

    fix: Check if startString is empty string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,18 @@
 export default function cleanSet(set, startString) {
+    // Si set n'est pas un Set, ou si startString n'est pas une string non vide,
+    // on retourne directement une chaîne vide.
+    if (!(set instanceof Set) || typeof startString !== 'string' || startString === '')
+    {
+    return '';
+    }
     // Initialiser un tableau pour stocker les parties filtrées
     const resultArray = [];
 
     // Parcourir chaque valeur du set
     for (const item of set) {
         // Vérifier si item commence par startString
-        // startsWith : Cette méthode de chaîne vérifie si la valeur commence par startString.
-        if (item.startsWith(startString) && startString !=='') {
+        // On s'assure que l'élément est une chaîne et qu'il commence par startString
+        if (typeof item === 'string' && item.startsWith(startString)) {
             // Ajouter à la liste uniquement la partie après startString
             // slice : Cela extrait la partie de la chaîne qui vient après startString.
             resultArray.push(item.slice(startString.length));
