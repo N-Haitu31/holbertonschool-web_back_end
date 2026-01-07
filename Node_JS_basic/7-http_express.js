af9072b22899afd6738b9add1df1f1bcbd3ef0ee
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-01-07 21:48:57 +0100

    fix: Replace on by at

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -18,7 +18,7 @@ app.get('/students', (req, res) => {
 });
 
 app.listen(1245, () => {
-  console.log('Server running on http://localhost:1245/');
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
